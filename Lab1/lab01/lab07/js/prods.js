acb9d12dfff240513c99d63521bfaf9a375107ef
TrungDuc9998 <81727369+TrungDuc9998@users.noreply.github.com>
2022-03-25 20:26:05 +0700

    upload

---
+++ Lab1/lab01/lab07/js/prods.js
@@ -0,0 +1,107 @@
+var list=[
+    {
+        "name":"Anissed Syrup",
+        "price":190,
+        "discount":0.19,
+        "date":"2000-03-16"
+    },
+    {
+        "name":"Change",
+        "price":19,
+        "discount":0,
+        "date":"1982-12-18"
+    },
+    {
+        "name":"Anissed Syrup",
+        "price":10,
+        "discount":0,
+        "date":"1973-06-09"
+    },
+    {
+        "name":"Chef Anton\u0027s Cajun Seasoning",
+        "price":22,
+        "discount":0,
+        "date":"1976-03-10"
+    },
+    {
+        "name":"Chef Anton\u0027s gumbo mix",
+        "price":21.35,
+        "discount":0,
+        "date":"1978-12-06"
+    },
+    {
+        "name":"grandma\u0027s Boysenberry Spread",
+        "price":25,
+        "discount":0.03,
+        "date":"1976-03-10"
+    },
+    {
+        "name":"grandma\u0027s Boysenberry Spread",
+        "price":25,
+        "discount":0.03,
+        "date":"1976-03-10"
+    },
+
+    {
+        "name":"điện thoại di động Oppo",
+        "price":23,
+        "discount":0.45,
+        "date":"1976-03-10"
+    },
+
+    {
+        "name":"máy tính gamming",
+        "price":50,
+        "discount":0.25,
+        "date":"1996-09-12"
+    },
+    {
+        "name":"máy tính bảng",
+        "price":100,
+        "discount":0.24,
+        "date":"1966-12-12"
+    },
+
+    {
+        "name":"zozo",
+        "price":189,
+        "discount":0.25,
+        "date":"1996-09-22"
+    },
+    {
+        "name":"danh sách sản phẩm",
+        "price":189,
+        "discount":0.25,
+        "date":"1996-09-22"
+    },
+    {
+        "name":"tab03",
+        "price":130,
+        "discount":0.25,
+        "date":"1996-09-22"
+    },
+    {
+        "name":"lập trình javascript",
+        "price":189,
+        "discount":0.25,
+        "date":"1996-09-22"
+    },
+    {
+        "name":"thiết kế hình ảnh với Photosshop",
+        "price":189,
+        "discount":0.25,
+        "date":"1996-09-22"
+    },
+    {
+        "name":"Cơ sở dữ liệu",
+        "price":189,
+        "discount":0.25,
+        "date":"1996-09-22"
+    },
+    {
+        "name":"Hệ quản trị cơ sở dữ liệu",
+        "price":182,
+        "discount":0.25,
+        "date":"1996-09-22"
+    },
+]
